901454ee2b82c064fe99db3524bc6d89f6676e76
Maudleen Imonirioma <maudleen.imoni@gmail.com>
2026-03-01 22:45:38 +0100

    Add health check endpoint for Render

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from .health import healthz
 
 urlpatterns = [
+    path("healthz/", healthz),
     path("admin/", admin.site.urls),
     path("api/auth/token/", obtain_auth_token, name="api-token"),
     path("api/", include("recipes.urls")),
